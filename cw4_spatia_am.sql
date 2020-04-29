8195c0008ef4dbeeac9859e253e638a3e7e9353b
shiroguru <48959257+shiroguru@users.noreply.github.com>
2020-04-30 00:13:44 +0200

    Update cw4_spatia_am.sql

--- cw4_spatia_am.sql
+++ cw4_spatia_am.sql
@@ -4,8 +4,7 @@ select mbrmaxy(geometry), name, elev from airportsnew limit 1
 select mbrminy(geometry), name, elev from airportsnew limit 1
 insert into airportsnew(name, geometry, elev) values('airportb', (0.5*distance ((select Geometry FROM airportsNew 
 where name='noatak'),(select geometry from airportsnew 
-where name='nikolski as') 
-)), (select avg(elev) from airportsnew
+where name='nikolski as') )), (select avg(elev) from airportsnew
 where name="nikolski as" or name="noatak"));
 select sum(area(intersection(tundra.geometry, trees.geometry))) as "tundra",
 sum(area(intersection(swamp.geometry, trees.geometry))) as "bagna", vegdesc
